ee8c805446841767b54403dc02fdf31f881d245d
khushboo <khushboovjvrg018@gmail.com>
2018-07-22 20:00:16 +0530

    import service

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,39 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import { ItemListService} from '../item-list.service';
+
 
 @Component({
-  selector: 'app-list',
-  templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+	selector: 'app-list',
+	templateUrl: './list.component.html',
+	styleUrls: ['./list.component.css'],
+	providers: [ItemListService]
 })
 export class ListComponent implements OnInit {
-list;
-text;
-  constructor() { }
+	list;
+	text;
+	oldItem;
+	state='default';
+	constructor(private _itemListService: ItemListService) { }
 
-  ngOnInit() {
-  	this.list = [{
-  		text : 'HTML'
-  	},
-  	{
-  		text : 'JS'
-  	}
-  	];
-  }
+	ngOnInit() {
+		this.list = this._itemListService.getItemList();
+	}
 
-  addItem(){
-  	console.log(this.text);
-  	this.list.push({
-  		text: this.text
-  	})
-  }
+	addItem(){
+		var newItem = {
+			text: this.text
+		};
+        this.list.push(newItem);
+		this._itemListService.addItem(newItem);
+		
+	}
 
-  deleteItem(item){
-  	for (var i = 0; i <this.list.length; i++) {
-  		if(this.list[i].text == item){
-  			this.list.splice(i,1);
-  	}
-  }
-  
+	deleteItem(item){
+		for (var i = 0; i <this.list.length; i++) {
+			if(this.list[i].text == item){
+				this.list.splice(i,1);
+			}
+		}
+       this._itemListService.deleteItem(item);
+	}
 
-}
+	editItem(list){
+		this.state = 'edit';
+		this.oldItem = list.text;
+		this.text = list.text;
+	}
+
+	updateItem(){
+		console.log(this.text);
+		for (var i = 0; i <this.list.length; i++) {
+			if(this.list[i].text == this.oldItem){
+				this.list[i].text= this.text;
+			}
+		}
+		this._itemListService.update(this.text,this.oldItem);
+	}
 }
