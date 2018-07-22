d0244cb0b087181b12a82a6df418e04fb8e4d658
khushboo <khushboovjvrg018@gmail.com>
2018-07-22 13:28:25 +0530

    some import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { ListComponent } from './list/list.component';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  
+
 })
 export class AppComponent {
   title = 'app';
